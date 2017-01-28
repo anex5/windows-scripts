5d5b4c6692c6c31c369f4a148ecf609b418a35f2
anex5 <anex5.2008@gmail.com>
2017-01-28 22:37:57 +0300

    Script fetches files from the internet with curl using regex pattern.
    Signed-off-by: anex5 <anex5.2008@gmail.com>

--- fetch.cmd
+++ fetch.cmd
@@ -1,15 +1,33 @@
 @if (@CodeSection == @Batch) @then
 @echo off & setlocal
+@chcp 65001
 
-set url=%1
-set filter=%2
-rem set "filter=himawari08_2017005_...._rgb_fd\.jpg"
+set URL=%1
+if not defined URL call :PRINT_USAGE&&goto:eof
 
-for /f "delims=" %%I in ('cscript /nologo /e:JScript "%~f0" "%url%" "%filter%"') do (
+set FILTER=%~2
+if not defined FILTER set "FILTER=*" 
+
+set DESTPATH=%~3
+if not defined DESTPATH set DESTPATH=%~dp0
+
+rem set "FILTER=himawari08_2017005_...._rgb_fd\.jpg"
+
+for /f "delims=" %%I in ('cscript /nologo /e:JScript "%~f0" "%URL%" "%FILTER%"') do (
     rem // do something useful with %%I
-    curl -o "f:\himawari\%%I" %url%%%I
+    set /a COUNTER+=1
+    echo.
+    echo !COUNTER!.%url%/%%I
+    curl -f -o "%DESTPATH%\%%I" %URI%/%%I    
 )
 pause
+
+:PRINT_USAGE
+   echo:
+   echo USAGE:
+   echo %~n0 ^<url^> ^<pattern^> [destination_path]
+exit /b !ERRORLEVEL!
+
 goto :EOF
 @end // end batch / begin JScript hybrid code
 
@@ -18,10 +36,10 @@ function fetch(url) {
     var XHR = WSH.CreateObject("Microsoft.XMLHTTP"),
         DOM = WSH.CreateObject('htmlfile');
 
-    XHR.open("GET",url,true);
-    XHR.setRequestHeader('User-Agent','XMLHTTP/1.0');
+    XHR.open("GET", url, true);
+    XHR.setRequestHeader('User-Agent','XMLHTTP/1.0' );
     XHR.send('');
-    while (XHR.readyState!=4) {WSH.Sleep(25)};
+    while (XHR.readyState!=4) { WSH.Sleep(25) };
     DOM.write('<meta http-equiv="x-ua-compatible" content="IE=9" />');
     DOM.write(XHR.responseText);
     return DOM;
