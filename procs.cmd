e1b23f45fae24f10d30aec7d6c025751dde6b11a
anex5 <anex5.2008@gmail.com>
2017-01-28 22:35:37 +0300

    Improvements. New function DAYNUM return Day number according to Julian calendar.
    Signed-off-by: anex5 <anex5.2008@gmail.com>

--- procs.cmd
+++ procs.cmd
@@ -29,11 +29,17 @@ exit /b
 
 	for /f "usebackq tokens=* delims=" %%I in ( `dir /b /s /a:-d "%SRCDIR%\*" 2^>NUL ^| findstr /i "%FILEMASKS%" 2^>NUL` ) do (
 		%PROC%
+
+		set PREVFILENAME=%%I
 		set /a COUNTER+=1
 		SETLOCAL ENABLEDELAYEDEXPANSION
 		set /a PTS= 100*!COUNTER!/!TOTAL!
-		echo.!COUNTER!^/!TOTAL! !PTS!^%% done.
-		echo.
+		if !ERRORLEVEL! NEQ 0 ( 
+    		@echo.Process failed. Error:!ERRORLEVEL!
+    	) else (
+    		@echo.!COUNTER!^/!TOTAL! !PTS!^%% done.
+			@echo.
+		)
 		endlocal
 	)
 	endlocal
@@ -45,11 +51,11 @@ exit /b !ERRORLEVEL!
 	set extract_cmd="%ProgramFiles%\WinRAR\winrar.exe"
 	set "rar_keys=-r -y -o- -ad -IBCK -INUL -x*\Thumbs.db"
 	set SRCDIR=%~dp1
-  set FILENAME=%~1
-  set DEST=%~2
-  set WORKPATH=%~3
-  call set DESTDIR=%%DEST%%%%SRCDIR:%WORKPATH%=%%
-  if not exist "%DESTDIR%" ( md "%DESTDIR%" || exit /b )
+    set FILENAME=%~1
+    set DEST=%~2
+    set WORKPATH=%~3
+    call set DESTDIR=%%DEST%%%%SRCDIR:%WORKPATH%=%%
+    if not exist "%DESTDIR%" ( md "%DESTDIR%" || exit /b )
 	echo.Extracting file "%FILENAME%"
 	%extract_cmd% x %rar_keys% -- "%FILENAME%" "%DESTDIR%" 2>NUL
 	if errorlevel 1 ( @echo.ERROR! Process %extract_cmd% failed. Code: %errorlevel% )
@@ -61,7 +67,7 @@ exit /b !ERRORLEVEL!
 	if not defined PSRCDIR set "PSRCDIR=" 
 	setlocal
 	set compress_cmd="%ProgramFiles%\WinRAR\winrar.exe"
-	set "rar_keys=-r -y -s -scul -p4chan.org -ep1 -m5 -ms*.bpg;*.jpg;*.png;*.rar;*.zip;*.7z -IBCK -INUL -x*\Thumbs.db"
+	set "rar_keys=-r -y -s -scul -ppassword -ep1 -m5 -ms*.bpg;*.jpg;*.png;*.rar;*.zip;*.7z -IBCK -INUL -x*\Thumbs.db"
 	set SRCDIR=%~dp1
 	set FILENAME="%~1"
 	set DEST=%~2
@@ -120,7 +126,7 @@ exit /b !ERRORLEVEL!
 	set sw=%~1
 	call set info=%%~%sw%2
 	::echo.info="%info%"
-	ENDLOCAL & IF "%~3" NEQ "" SET %~3=%info%
+	ENDLOCAL & IF "%~3" NEQ "" SET %3=%info%
 exit /b !ERRORLEVEL!
 
 :ESCAPE_SCHARS_STR
@@ -130,13 +136,65 @@ exit /b !ERRORLEVEL!
 	set str=%~1
 	for %%f in (%schars%) do (
     set str="!str:%%f=^%%f!"  
-	ENDLOCAL & IF "%~2" NEQ "" SET %~2=%str%                  
+	ENDLOCAL & IF "%~2" NEQ "" SET %2=%str%                  
+exit /b !ERRORLEVEL!
+
+:FN_TO_FFCONCAT
+	if [%1] == [] call :ERROR_ARG %*
+	setlocal ENABLEDELAYEDEXPANSION
+	set FILENAME=%~1
+	set PREVFILENAME="%~2"
+	set FFCONCATFILENAME="%~3"
+
+	set /a nh=0 & set /a nm=0
+	set /a ph=23 & set /a pm=50
+
+	for /f "delims=_, tokens=3,4" %%I in ( "%PREVFILENAME%" ) do ( 
+		set p=%%I
+		set ph=!p:~0,2!
+		set pm=!p:~2,4!
+		set /a ph=100!ph! %% 100
+		set /a pm=100!pm! %% 100
+    )
+
+	for /f "delims=_, tokens=3,4" %%I in ( "%FILENAME%" ) do ( 
+		set n=%%I
+		set nh=!n:~0,2!
+		set nm=!n:~2,4!
+		set /a nh=100!nh! %% 100
+		set /a nm=100!nm! %% 100
+    )
+
+    set /a DELAY=((nh-ph)*60+(nm-pm))
+
+    echo !DELAY! !nh! !ph! !nm! !pm! %PREVFILENAME% %FFCONCATFILENAME%
+    if !DELAY!==-1430 set /a DELAY=10
+        
+	echo file !FILENAME:\=\\!>>%FFCONCATFILENAME%
+	echo duration !DELAY:~0,-1!>>%FFCONCATFILENAME% 
+
+	ENDLOCAL
 exit /b !ERRORLEVEL!
 
 :NEWDATENAME
     for /f "delims=:., tokens=1-4" %%m in ("%TIME: =0%") do (
-        echo %1-%date:~6,4%-%date:~3,2%-%date:~0,2%-%%m%%n%%o%%p
+        @echo.%~1-%date:~6,4%-%date:~3,2%-%date:~0,2%-%%m%%n%%o%%p%~2
     )
+exit /b !ERRORLEVEL!
+
+:DAYNUM
+	setlocal
+	for /F "tokens=1-3 delims=." %%a in ( "%~1" ) do (
+   		set /A "DD=1%%a-100, MM=1%%b-100, Ymod4=%%c%%4 "
+	)
+
+	for /F "tokens=%MM%" %%m in ("0 31 59 90 120 151 181 212 243 273 304 334") do (
+		set /A DN=DD+%%m
+	)
+	if %Ymod4% equ 0 if %MM% gtr 2 set /A DN+=1
+	set SDN=00%DN%
+	@echo.%SDN:~-3%
+	ENDLOCAL
 exit /b !ERRORLEVEL!
 
 :UNQ
