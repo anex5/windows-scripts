37b46a4af1e22c5e84972084fcf095d7fee7f199
anex5 <anex5.2008@gmail.com>
2017-02-21 19:16:00 +0300

    utf-8 encoding. bugfixes.
    Signed-off-by: anex5 <anex5.2008@gmail.com>

--- install firefox esr.cmd
+++ install firefox esr.cmd
@@ -1,4 +1,5 @@
 @echo off
+@chcp 65001
 setlocal enableextensions enabledelayedexpansion
 
 set SRCDIR=%~1
@@ -7,7 +8,7 @@ if not defined SRCDIR set SRCDIR=%~dp0
 set "PROFILEDIR=%APPDATA%\Mozilla\Firefox\Profiles\"
 
 set FILEMASKS="FirefoxSetup.exe"
-for /f "usebackq tokens=* delims=" %%I in ( `dir /b /s /a:-d "%SRCDIR%\*" 2^>NUL ^| findstr /i "%FILEMASKS%" 2^>NUL` ) do (
+for /f "usebackq tokens=* delims=" %%I in ( `dir /b /s /a:-d "%SRCDIR%\*" 2^>NUL ^| findstr /I "%FILEMASKS%" 2^>NUL` ) do (
     set INSTALLER="%%I"
     set INSTALLER_EXENAME="%%~nxI"
     set INSTALLER_PATH="%%~dpI"
@@ -17,24 +18,28 @@ set "URI=https://download.mozilla.org/?product=firefox-esr-latest&os=win64&lang=
 
 if not exist "%INSTALLER%" ( 
     if "!INSTALLER_EXENAME!" EQU "" ( 
-      set INSTALLER_EXENAME="FirefoxSetup.exe" 
+      set "INSTALLER_EXENAME=FirefoxSetup.exe" 
       set INSTALLER_PATH=%SRCDIR%
       set "INSTALLER=!SRCDIR!!INSTALLER_EXENAME!"
     ) 
+    @echo.
     @echo.Загружаю установщик !URI!...
     if "!URI!" NEQ "" ( start "" /b /w curl.exe -L "!URI!" -o "!INSTALLER_EXENAME!" ) else ( @echo.Cсылка на инсталлятор не найдена )
 )
 
-@echo.Устанавливаю waterfox !INSTALLER!
+@echo.
+@echo.Устанавливаю Firefox ESR !INSTALLER!
 if exist "%INSTALLER%" ( start "" /b /w "%INSTALLER%" -ms ) else ( @echo.Инсталлятор не найден в директории %SRCDIR%&goto:eof )
 if exist "%ProgramFiles%\Mozilla Firefox\Firefox.exe" ( start "" /b /w "%ProgramFiles%\Mozilla Firefox\Firefox.exe" -CreateProfile "%UserName%" )
-for /f "usebackq tokens=* delims=" %%I in ( `dir /b "%PROFILEDIR%\*" 2^>NUL ^| findstr /i "%USERNAME%" 2^>NUL` ) do (
-    set "PROFILENAME=%%I"   
+
+for /f "usebackq tokens=* delims=" %%I in ( `dir /b %PROFILEDIR%\ 2^>NUL ^| find /I "%USERNAME%" 2^>NUL` ) do (
+    set "PROFILENAME=%%I"
 )
-rem @echo.!PROFILENAME!
+
+@echo.
+@echo.Профиль пользователя создан: !PROFILEDIR!!PROFILENAME!
 if exist "%INSTALLER_PATH%\prefs.js" copy "%INSTALLER_PATH%\prefs.js" "!PROFILEDIR!!PROFILENAME!\"
+if exist "%INSTALLER_PATH%\userContent.css" copy "%INSTALLER_PATH%\userContent.css" "!PROFILEDIR!!PROFILENAME!\chrome\"
 
 if !ERRORLEVEL! NEQ 0 ( @echo.Выполнение прервано. Ошибка:!errorlevel! )
 echo.&goto:eof
-
-
